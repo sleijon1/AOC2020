5e2e2e997e8e351adf64e77e4552eedf4e0b9ca1
sleijon1 <simonleijon59@gmail.com>
2020-12-02 08:05:19 +0100

    cleanup

--- day2/day2.py
+++ day2/day2.py
@@ -1,13 +1,12 @@
 lines = open("input.txt").read().splitlines()
 valid_passwords_1 = valid_passwords_2 = 0
 for line in lines:
-    occ, char, pw = line.split(" ")
-    lb, ub = occ.split("-")
-    char = char[0]
+    occ, char, pw = line.replace(":","").split(" ")
+    lb, ub = map(int, occ.split("-"))
     act_occ = pw.count(char)
-    if int(lb) <= act_occ <= int(ub):
+    if lb <= act_occ <= ub:
         valid_passwords_1 += 1
-    if sum([pw[int(lb)-1] == char, pw[int(ub)-1] == char]) == 1:
+    if sum([pw[lb-1] == char, pw[ub-1] == char]) == 1:
         valid_passwords_2 += 1
 print("Solution part 1:", valid_passwords_1)
 print("Solution part 2:", valid_passwords_2)
