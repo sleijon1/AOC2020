6784e542c63494852b510a7e3ee1957181331918
sleijon1 <simonleijon59@gmail.com>
2020-12-07 08:29:27 +0100

    d7 done

--- day7/day7.py
+++ day7/day7.py
@@ -5,7 +5,6 @@ for rule in rules:
     parent, children = rule.split('contain')
     for child in children.split(','):
         rules_dict[parent.strip(' ')].append(child.strip('. '))
-print(rules_dict)
 
 
 def shiny_gold_bag(parent: str) -> int:
@@ -16,14 +15,27 @@ def shiny_gold_bag(parent: str) -> int:
     for child in rules_dict[parent]:
         _, att_1, att_2, bags = child.split(' ')
         key = att_1 + ' ' + att_2 + ' ' + bags
-        if bags[-1] == 'g':
-            key += 's'
+        key += 's' if bags[-1] == 'g' else ''
         if (shiny_gold_bag(key)):
             return 1
     return 0
 
 
+def contains(parent: str, total_bags=0) -> int:
+    if 'no other bags' in rules_dict[parent]:
+        return total_bags
+    for child in rules_dict[parent]:
+        no_bags, att_1, att_2, bags = child.split(' ')
+        key = att_1 + ' ' + att_2 + ' ' + bags
+        key += 's' if bags[-1] == 'g' else ''
+        for i in range(int(no_bags)):
+            total_bags += (1 + contains(key))
+    return total_bags
+
+
 containers = 0
 for parent in rules_dict:
     containers += shiny_gold_bag(parent)
-print(containers)
+sg_bags = contains('shiny gold bags')
+print(f"Solution part 1: {containers}")
+print(f"Solution part 2: {sg_bags}")
