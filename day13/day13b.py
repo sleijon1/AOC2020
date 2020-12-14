b526eeee8c2b790df346ac2a10669ef74e3a23e8
sleijon1 <simonleijon59@gmail.com>
2020-12-14 19:15:08 +0100

    part 2 done (crt)

--- day13/day13b.py
+++ day13/day13b.py
@@ -1,21 +1,22 @@
-earliest, busses = open("input.txt").read().splitlines()
-earliest = int(earliest)
-bus_list = busses.split(',')
-in_service = [int(bus) for bus in bus_list if bus != 'x']
-first = in_service[0]
-last = max(in_service)
-distance_from_max = [bus_list.index(str(id_))-bus_list.index(str(last))
-                     for id_ in in_service]
-print(last)
+from functools import reduce
+import sys
+sys.path.append('../../')
+from pyutils.math import crt
 
-last = (100000000000000//last)*last
-while True:
-    found = True
-    for i, id_ in enumerate(in_service):
-        if not ((last + distance_from_max[i]) % id_ == 0):
-            found = False
+_, busses = open("input.txt").read().splitlines()
+bus_list = busses.split(',')
+
+in_service = [int(bus) for bus in bus_list if bus != 'x']
+distance = [bus_list.index(str(in_service[0]))-bus_list.index(str(id_))
+                     for id_ in in_service]
+remainder = [id_ + dist for id_, dist in zip(in_service, distance)]
+for i, r in enumerate(remainder):
+    while True:
+        if remainder[i] < 0:
+            remainder[i] += in_service[i]
+        else:
             break
-    if found:
-        break
-    last += max(in_service)*(len(in_service)-1)
-print(f"Solution part 2: {last-bus_list.index(str(max(in_service)))}")
+remainder[0] = 0
+
+solution = crt.crt(in_service, remainder)
+print(f"Solution part 2: {solution}")
